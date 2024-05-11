4aa3ca6b4e01047cbb9c7e67c3db2407bfefc3e4
senvildev <realsenvil@gmail.com>
2024-05-11 18:41:23 +0200

    added comments to the backend

--- private/backend.js
+++ private/backend.js
@@ -2,17 +2,21 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 
 
-
+// create a backend server
 function start_backend(HOSTNAME, PORT)
 {
+	// declare the backend
 	const backend = new Elysia();
-	backend.use(cors()); // add cors
+	backend.use(cors()); // use cors
 
+	// listen on the parsed port and hostname
 	backend.listen({ port: PORT, hostname: HOSTNAME });
 
-	if(backend) console.log(`[!] started backend on http://${HOSTNAME}:${PORT}/`);
+	// print out the address if the backend initialized successfully
+	if (backend) console.log(`[!] started backend on http://${HOSTNAME}:${PORT}/`);
 }
 
+// export the function
 export {
 	start_backend
 }
