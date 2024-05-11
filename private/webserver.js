170d1fdd6c5e1b640a004848eb356f721aee509d
senvildev <realsenvil@gmail.com>
2024-05-11 18:43:01 +0200

    switched to elysia, added error handling

--- private/webserver.js
+++ private/webserver.js
@@ -1,31 +1,63 @@
 // import required frameworks etc
-import { serve, file } from "bun";
+import { file } from "bun";
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { staticPlugin } from "@elysiajs/static";
 
-const URL_DIRECTORIES = ['/', '/home'];
+import { no_caching } from "./modules/middleware-functions";
 
+// start the webserver
 function start_webserver(HOSTNAME, PORT)
 {
-    const webserver = serve({
-        host: HOSTNAME,
-        port: PORT,
+	// declare the webserver
+	const webserver = new Elysia();
 
-        fetch(request)
-        {
-            const url = new URL(request.url);
-			if(URL_DIRECTORIES.includes(url.pathname)) {
-				if(url.pathname == '/') return Response.redirect('/home', 301)
-				url.pathname += '.html';
-				return new Response(file(`public${url.pathname}`));
-			}
-			if(url.pathname.includes("js/") || url.pathname.includes("css/") || url.pathname.includes("src/"))
-				return new Response(file(`public${url.pathname}`));
-			return new Response("404");
-        }
-    })
+	webserver.use(cors()); // use cors
+	webserver.use(no_caching(webserver)); // disable caching
 
-    if(webserver) console.log(`[!] started webserver on http://${HOSTNAME}:${PORT}/`);
+	// declare the static folder
+	webserver.use(staticPlugin({ assets: 'public', prefix: '' }));
+
+	// redirect the user to /home if they just put the link in
+	webserver.get("/", (request) => {
+		return Response.redirect("/home");
+	});
+	
+	// return the homepage file
+	webserver.get("/home", (request) => {
+		return file("public/home.html");
+	});
+
+	// return the product page
+	webserver.get("/offer/:id", (request) => {
+		// get the offer id
+		const offer_id = request.params.id;
+
+		// if the offer id is correct
+		if (offer_id)
+		{
+			// check if the offer exists
+			const offer_exists = true;
+			// return the product file if exists
+			if (offer_exists)
+				return file("public/offer.html")
+			else return 404;
+		} else return 404; // otherwise doesn't exist
+	});
+
+	// return the error's code if an error occurs
+	webserver.onError(({ code }) => {
+		return code
+	});
+
+	// listen on the parsed hostname and port
+	webserver.listen({ host: HOSTNAME, port: PORT });
+
+	// print the address of the webserver if it was created successfully
+    if (webserver) console.log(`[!] started webserver on http://${HOSTNAME}:${PORT}/`);
 }
 
+// export the main function
 export {
     start_webserver
 }
