cbc8257c6e339a78e63d85fc0f63b26cb02004c9
senvildev <realsenvil@gmail.com>
2024-05-11 18:40:56 +0200

    updated main server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import { start_webserver } from "./private/webserver";
 import { start_backend } from "./private/backend";
+import { start_cdn } from "./private/cdn";
 
 import {
 	HOSTNAME,
-	BACKEND_PORT, WEBSERVER_PORT, WEBSOCKET_PORT
+	BACKEND_PORT, WEBSERVER_PORT, WEBSOCKET_PORT, CDN_PORT
 } from "./config.json";
 
 start_backend(HOSTNAME, BACKEND_PORT);
+start_cdn(HOSTNAME, CDN_PORT);
 start_webserver(HOSTNAME, WEBSERVER_PORT);
