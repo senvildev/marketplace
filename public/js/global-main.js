bff4c3cf135ad51bf8288dfdeed0c06174c2431f
senvildev <realsenvil@gmail.com>
2024-05-13 17:55:18 +0200

    improved already existing frontend parts

--- public/js/global-main.js
+++ public/js/global-main.js
@@ -4,16 +4,40 @@
 PAGE_Y_OFFSET = window.scrollY;
 
 // when page is finished scrolling
-document.addEventListener("scroll", (event) => {
-	// get the new Y offset of the document
-	const new_page_y_offset = window.scrollY;
+if (!IGNORE_PATHS.includes(window.location.pathname))
+{
+	document.addEventListener("scroll", (event) => {
+		// get the new Y offset of the document
+		const new_page_y_offset = window.scrollY;
+	
+		// run the function to check whether the
+		// header should hide or appear
+		show_header(
+			new_page_y_offset > PAGE_Y_OFFSET ? true : false
+		);
+	
+		// override the global variable
+		PAGE_Y_OFFSET = new_page_y_offset;
+	});
+}
 
-	// run the function to check whether the
-	// header should hide or appear
-	show_header(
-		new_page_y_offset > PAGE_Y_OFFSET ? true : false
-	);
+// replace all svg images with svg elements
+async function replace_img_svg()
+{
+	const svg_images = document.querySelectorAll("img#svg-image");
+	for (const element of svg_images)
+	{
+		const parent = element.parentElement;
+		const image_source = element.src;
 
-	// override the global variable
-	PAGE_Y_OFFSET = new_page_y_offset;
-})
+		const svg_element_temp = document.createElement("svg");
+		const svg_data = await fetch(image_source);
+		const parsed_svg_data = await svg_data.text();
+		svg_element_temp.innerHTML = parsed_svg_data;
+		const svg_element = svg_element_temp.childNodes[0];
+		console.log(svg_element)
+
+		element.replaceWith(svg_element);
+	}
+}
+replace_img_svg();
--- public/js/global-variables.js
+++ public/js/global-variables.js
@@ -2,3 +2,14 @@
 
 let PAGE_Y_OFFSET;
 let HEADER_BAR_VISIBLE = true;
+
+const MONTHS = [
+	"styczeń", "luty", "marzec",
+	"kwiecień", "maj", "czerwiec",
+	"lipiec", "sierpień", "wrzesień",
+	"październik", "listopad", "grudzień"
+];
+
+const IGNORE_PATHS = [
+	"/login"
+]
