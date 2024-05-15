369bfead9236bd5eca8c165ec431142cc168e86c
senvildev <realsenvil@gmail.com>
2024-05-15 19:33:02 +0200

    added more to the frontend

--- public/js/dom-elements/global.js
+++ public/js/dom-elements/global.js
@@ -1,4 +1,14 @@
 // global elements used across the site
 
 // header element
-const header = document.querySelector("header");
+const header_element = document.querySelector("header");
+
+// header buttons
+const saved_offers_button_element =
+	document.getElementById("saved_offers");
+const messages_button_element =
+	document.getElementById("messages");
+const profile_button_element =
+	document.getElementById("profile");
+const create_offer_button_element =
+	document.getElementById("create_offer");
