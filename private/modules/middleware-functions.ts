078e59da6c7976df65de51e1aae2a05e5be5ff66
senvildev <realsenvil@gmail.com>
2024-05-15 19:32:41 +0200

    moved middleware

--- private/modules/middleware-functions.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia, t } from "elysia";
-
-function no_caching(app : Elysia) {
-	return app.onRequest(({ set }) => {
-		set.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-	});
-}
-
-export { no_caching }
