ad3b500a64e63f1cb74daa4d16ed4f8788147b98
senvildev <realsenvil@gmail.com>
2024-05-15 19:32:09 +0200

    added listeners

---
+++ public/js/listeners/global.js
@@ -0,0 +1,15 @@
+saved_offers_button_element.addEventListener("mouseup", () => {
+	
+});
+
+messages_button_element.addEventListener("mouseup", () => {
+
+});
+
+profile_button_element.addEventListener("mouseup", () => {
+
+});
+
+create_offer_button_element.addEventListener("mouseup", () => {
+
+});
